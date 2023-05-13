f7c4a1d21367efe025ae4ce94d44f69006d3a13e
cherrynoize <cherrynoize9987@outlook.com>
2023-05-13 04:31:02 +0200

    fixed nav

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -3,25 +3,27 @@ import { Link } from "react-router-dom";
 
 export default function Nav() {
   return (
-    <nav>
-      <Link to="/">
-        <button>
-          Home
-        </button>
-      </Link>
-      <a className="button" target="_blank" href="gallery">
-        Gallery
-      </a>
-      <Link to="cool-stuff">
-        <button>
-          Cool shit
-        </button>
-      </Link>
-      <Link to="blog">
-        <button>
-          Blog
-        </button>
-      </Link>
-    </nav>
+    <div className="nav-container">
+      <nav>
+        <Link to="/">
+          <button>
+            Home
+          </button>
+        </Link>
+        <a className="button" href="gallery">
+          Gallery
+        </a>
+        <Link to="cool-stuff">
+          <button>
+            Cool shit
+          </button>
+        </Link>
+        <Link to="blog">
+          <button>
+            Blog
+          </button>
+        </Link>
+      </nav>
+    </div>
   );
 }
