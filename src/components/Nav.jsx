456b19a3b8918576f65f7cb11873fafa699f75d0
cherrynoize <cherrynoize9987@outlook.com>
2023-05-14 10:33:30 +0200

    .

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -3,27 +3,25 @@ import { Link } from "react-router-dom";
 
 export default function Nav() {
   return (
-    <div className="nav-container">
-      <nav>
-        <Link to="/">
-          <button>
-            Home
-          </button>
-        </Link>
-        <a className="button" href="gallery">
-          Gallery
-        </a>
-        <Link to="cool-stuff">
-          <button>
-            Cool shit
-          </button>
-        </Link>
-        <Link to="blog">
-          <button>
-            Blog
-          </button>
-        </Link>
-      </nav>
-    </div>
+    <nav>
+      <Link to="/">
+        <button>
+          Home
+        </button>
+      </Link>
+      <a className="button" href="gallery">
+        Gallery
+      </a>
+      <Link to="cool-stuff">
+        <button>
+          Cool shit
+        </button>
+      </Link>
+      <Link to="blog">
+        <button>
+          Blog
+        </button>
+      </Link>
+    </nav>
   );
 }
