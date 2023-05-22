d1e0180e8de2bda372fc6fb26467b9fd8f35219f
cherrynoize <cherrynoize9987@outlook.com>
2023-05-22 21:36:08 +0200

    fixed contribute links and panel captions

--- src/pages/Contribute.jsx
+++ src/pages/Contribute.jsx
@@ -1,31 +1,31 @@
 import Caption from '../components/Caption';
 
-function copyToClipboard(token) {
-  try {
-    let res = document.execCommand('copy');
-    let msg = res ? 'success' : 'failed';
-    console.log('Copying ' + msg);
-  } catch (err) {
-    console.log('Error: Oops, unable to copy!');
-  }
+function copyToClipboard(txt) {
+  navigator.clipboard.writeText(txt);
 }
 
-function WalletAddress(props) {
+function Address(props) {
   const token = props.token;
   const address = props.children;
+  const title = props.title;
+  const copy = props.copy;
 
   const handleClick = e => {
-    if (e.detail === 2) // double click
-      return copyToClipboard(token);
+    // click type and title prop
+    if ((e.detail === 2) && copy)
+      return copyToClipboard(address);
   };
 
   return (
-    <p className='address-container' onClick={handleClick}>
+    <p className='address-container'
+      onClick={handleClick}
+    >
       <b>{token.toUpperCase()}</b>
       <button
         className='a address-text'
         id={token + '-address'}
-        title="Copy"
+        title={title}
+        copy={copy}
       >
         {address}
       </button>
@@ -42,35 +42,28 @@ export default function Contacts() {
             If you want to show your support you can buy me a
             Coke:
           </p>
-
           <a target="_blank" rel="noreferrer" href="https://ko-fi.com/cherrynoize">
-            <WalletAddress token='pypl'>
+            <Address token='pypl'>
               Ko-fi.com/cherrynoize
-            </WalletAddress>
+            </Address>
           </a>
-
           <a target="_blank" rel="noreferrer" href="https://paypal.me/cherrynoize">
-            <WalletAddress token='pypl'>
+            <Address token='pypl'>
               PayPal.me/cherrynoize
-            </WalletAddress>
+            </Address>
           </a>
-
-          <WalletAddress token='eth'>
+          <Address token='eth' title="Click twice to copy" copy>
             0x5938C4DA9002F1b3a54fC63aa9E4FB4892DC5aA8
-          </WalletAddress>
-
-          <WalletAddress token='sol'>
+          </Address>
+          <Address token='sol' title="Click twice to copy" copy>
             G77bErQLYatQgMEXHYUqNCxFdUgKuBd8xsAuHjeqvavv
-          </WalletAddress>
-
-          <WalletAddress token='bnb'>
+          </Address>
+          <Address token='bnb' title="Click twice to copy" copy>
             0x0E0eAd7414cFF412f89BcD8a1a2043518fE58f82
-          </WalletAddress>
-
-          <WalletAddress token='lunc'>
+          </Address>
+          <Address token='lunc' title="Click twice to copy" copy>
             terra1n5sm6twsc26kjyxz7f6t53c9pdaz7eu6zlsdcy
-          </WalletAddress>
-
+          </Address>
           <h5>
             Thank you for using my stuff.
           </h5>
